a0a3f8a1be59ee780b16bbb85edc993cd6088f91
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-12-17 22:14:29 -0600

    changed database-url

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = b'\xd6\xb3\x18\xed\x84\x8b\xfc\t\xaamW\xe0\x9c\x07\xfd\xc7\xbf\x1cW\x9dm\xaa>\x8b'
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/operation_chris"
+    SQLALCHEMY_DATABASE_URI = "postgres://wkikkpvhhsfhda:4b041766d1f68c5aee188e0c080c1eb1d39bbd3459503c19adea89863ecb5a43@ec2-54-235-244-185.compute-1.amazonaws.com:5432/d1tf3le2r6ka19"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     print(SQLALCHEMY_DATABASE_URI)
 
